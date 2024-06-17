c7910507ba4ef6d1c0fc743670475cd0742d3143
Lishakar <lishakarj@gmail.com>
2024-06-17 08:52:46 +0530

    User Service Initial Template

---
+++ src/test/java/com/splitwise/User/Service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.User.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
